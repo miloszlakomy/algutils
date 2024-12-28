7a4d7e29f5116e29ba1e20275c0a5ebf6763dfc1
Milosz Lakomy <milosz.lakomy@gmail.com>
2024-12-28 11:02:07 +0100

    Update dynamic_print.py - Rename __DynamicPrintSingleton to _DPS

--- dynamic_print.py
+++ dynamic_print.py
@@ -1,31 +1,30 @@
 import inspect
 import io
+import pprint as pp
 import re
 import shutil
 from typing import Any
 
 
 def dprint(name: str, value: Any = None) -> None:
-    if value is None:
-        value = _parent_locals()[name]
-
-    _DynamicPrintSingleton.set_name_to_value(name, value)
-    _DynamicPrintSingleton.dprint()
+    _DPS.dprint(name, value)
 
 
 def finish() -> None:
-    _DynamicPrintSingleton.finish()
+    _DPS.finish()
 
 
 def clear_and_finish() -> None:
-    _DynamicPrintSingleton.clear_and_finish()
+    _DPS.clear_and_finish()
 
 
-def _parent_locals(depth:int = 1) -> dict[str, Any]:
-    return inspect.getouterframes(inspect.currentframe())[depth+1][0].f_locals
+def _parent_locals(depth: int = 1) -> dict[str, Any]:
+    return inspect.getouterframes(inspect.currentframe())[depth + 1][0].f_locals
 
 
-class _DynamicPrintSingleton:
+class _DPS:
+    """Dynamic print singleton"""
+
     _names: list[str] = []
     _names_to_values: dict[str, str] = {}
     _last_print: str = ""
@@ -35,44 +34,52 @@ class _DynamicPrintSingleton:
 
     @staticmethod
     def set_name_to_value(name: str, value: Any) -> None:
-        str_value = str(value)
-
-        if name not in _DynamicPrintSingleton._names_to_values:
-            _DynamicPrintSingleton._names.append(name)
+        if name not in _DPS._names_to_values:
+            _DPS._names.append(name)
 
         last_value_length = 0
-        if name in _DynamicPrintSingleton._names_to_values:
-            last_value_length = len(_DynamicPrintSingleton._names_to_values[name])
+        if name in _DPS._names_to_values:
+            last_value_length = len(_DPS._names_to_values[name])
+
+        if isinstance(value, str):
+            str_value = value
+        else:
+            str_value = pp.pformat(value)
 
         # Overwrite last value with space characters
-        _DynamicPrintSingleton._names_to_values[name] = str_value + " " * (
+        _DPS._names_to_values[name] = str_value + " " * (
             last_value_length - len(str_value)
         )
 
     @staticmethod
-    def dprint() -> None:
-        _DynamicPrintSingleton._clear()
-        _DynamicPrintSingleton._reset_cursor()
-        _DynamicPrintSingleton._print_all()
+    def dprint(name: str, value: Any = None) -> None:
+        if value is None:
+            value = _parent_locals()[name]
+
+        _DPS.set_name_to_value(name, value)
+
+        _DPS._clear()
+        _DPS._reset_cursor()
+        _DPS._print_all()
 
     @staticmethod
     def finish() -> None:
-        _DynamicPrintSingleton._names.clear()
-        _DynamicPrintSingleton._names_to_values.clear()
-        _DynamicPrintSingleton._last_print = ""
+        _DPS._names.clear()
+        _DPS._names_to_values.clear()
+        _DPS._last_print = ""
 
     @staticmethod
     def clear_and_finish() -> None:
-        _DynamicPrintSingleton._clear()
-        _DynamicPrintSingleton._reset_cursor()
-        _DynamicPrintSingleton.finish()
+        _DPS._clear()
+        _DPS._reset_cursor()
+        _DPS.finish()
 
     @staticmethod
     def _clear() -> None:
-        _DynamicPrintSingleton._reset_cursor()
+        _DPS._reset_cursor()
         # re ignores newlines
         print(
-            re.sub(pattern=".", repl=" ", string=_DynamicPrintSingleton._last_print),
+            re.sub(pattern=".", repl=" ", string=_DPS._last_print),
             end="",
         )
 
@@ -80,8 +87,8 @@ class _DynamicPrintSingleton:
     def _print_all() -> None:
         stream = io.StringIO()
 
-        for name in _DynamicPrintSingleton._names:
-            value = _DynamicPrintSingleton._names_to_values[name]
+        for name in _DPS._names:
+            value = _DPS._names_to_values[name]
             stream.write(f"{name}={value}\n")
 
         print(
@@ -89,13 +96,11 @@ class _DynamicPrintSingleton:
             end="",
         )
 
-        _DynamicPrintSingleton._last_print = stream.getvalue()
+        _DPS._last_print = stream.getvalue()
 
     @staticmethod
     def _reset_cursor() -> None:
-        _DynamicPrintSingleton._move_cursor_up(
-            number_of_lines=_str_height(_DynamicPrintSingleton._last_print)
-        )
+        _DPS._move_cursor_up(number_of_lines=_str_height(_DPS._last_print))
 
     @staticmethod
     def _move_cursor_up(number_of_lines: int) -> None:
