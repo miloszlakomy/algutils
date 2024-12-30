bd611a7e114e2ec26b269dba6ac2eca4f4d05e19
Milosz Lakomy <milosz.lakomy@gmail.com>
2024-12-30 09:23:21 +0100

    Update color_print.py - Add terminal color support detection

--- color_print.py
+++ color_print.py
@@ -2,9 +2,12 @@ import pprint as pp
 from typing import Any, Optional
 
 import pygments
+import pygments.formatter
 import pygments.formatters
 import pygments.lexer
 import pygments.lexers
+import pygments.style
+import rich.console
 
 
 def cprint(value: Any) -> None:
@@ -32,6 +35,7 @@ def _highlight(
     value: Any,
     lexer: Optional[pygments.lexer.Lexer] = None,
     value_type: Optional[type] = None,
+    style: pygments.style.Style = "borland",
 ) -> str:
     if lexer is None:
         lexer = _guess_lexer(
@@ -44,7 +48,7 @@ def _highlight(
     highlighted_str = pygments.highlight(
         code=str_value,
         lexer=lexer,
-        formatter=pygments.formatters.TerminalFormatter(),
+        formatter=_determine_formatter(style=style),
     )
 
     return highlighted_str.removesuffix("\n")
@@ -74,3 +78,18 @@ def _guess_lexer(value: Any, value_type: Optional[type]) -> pygments.lexer.Lexer
         return lexer
 
     return pygments.lexers.PythonLexer()
+
+
+def _determine_formatter(style: pygments.style.Style) -> pygments.formatter.Formatter:
+    color_support = rich.console.Console().style
+
+    if color_support == "truecolor":
+        formatter_cls = pygments.formatters.TerminalTrueColorFormatter
+    elif color_support == "256":
+        formatter_cls = pygments.formatters.Terminal256Formatter
+    else:
+        formatter_cls = pygments.formatters.TerminalFormatter
+
+    formatter = formatter_cls(style=style)
+
+    return formatter
