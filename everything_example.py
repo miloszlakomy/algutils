6d0c6474250972934f7ca27ff15318407ef39a11
Milosz Lakomy <milosz.lakomy@gmail.com>
2025-02-01 18:17:06 +0100

    Add everything.py

---
+++ everything_example.py
@@ -0,0 +1,12 @@
+from everything import *
+
+
+def main() -> None:
+    print("`math' and `string' were never imported?!")
+
+    print(math.pi)
+    print(string.ascii_lowercase)
+
+
+if __name__ == "__main__":
+    main()
