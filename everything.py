8e7d5e164dde4bd3dc21c1e2dd431b7fcb5d4b9b
Milosz Lakomy <milosz.lakomy@gmail.com>
2025-02-28 09:27:30 +0100

    Update everything.py - Use Switcheroo as base class. Rework populating lazy child modules. Add typing and nptyping imports

--- everything.py
+++ everything.py
@@ -2,7 +2,11 @@ import importlib
 import itertools
 import os
 import sys
+import types
 from typing import Any
+
+import algutils.switcheroo
+import algutils.deep_getattr
 
 
 class _Everything:
@@ -17,46 +21,50 @@ class _Everything:
             globals()[name] = value
 
 
-    class _LazyImport:
-        def __init__(self, name: str, parent) -> None:
-            self._name = name
-            self._parent = parent
+    class _LazyImport(algutils.switcheroo.Switcheroo):
+        def __init__(self, name: str) -> None:
+            super().__init__()
 
-        def __getattr__(self, attribute_name: str) -> Any:
+            self._name = name
+
+        def _load_switch_to(self, _unused_attribute_name: str) -> Any:
             module = importlib.import_module(self._name)
-            edge = self._name.rpartition(".")[-1]
-            setattr(self._parent, edge, module)
+            _Everything._LazyImport._populate_module_children(module=module)
+            return module
+
+        @staticmethod
+        def _populate_module_children(module: types.ModuleType) -> None:
             for child_name in _Everything._get_module_children_names(module=module):
-                _Everything._LazyImport._lazy_import(name=child_name)
-            return getattr(module, attribute_name)
+                child_edge = child_name.rpartition(".")[-1]
+                if child_edge in dir(module):
+                    continue
+                lazy_import = _Everything._LazyImport(name=child_name)
+                setattr(module, child_edge, lazy_import)
 
         @staticmethod
         def _lazy_import(name: str) -> None:
             path = name.split(".")
+            if len(path) == 1 and name not in globals():
+                globals()[name] = _Everything._LazyImport(name=name)
+                return
+
             parent = _Everything._Globals()
             for depth, edge in enumerate(path):
+                parent = getattr(parent, edge)
                 if isinstance(parent, _Everything._LazyImport):
                     return
-                try:
-                    parent = getattr(parent, edge)
-                    # Returns if the entire name has been traversed.
-                except AttributeError:
-                    lazy_import = _Everything._LazyImport(
-                        name=".".join(path[: depth + 1]),
-                        parent=parent,
-                    )
-                    setattr(parent, edge, lazy_import)
-                    return
+                _Everything._LazyImport._populate_module_children(module=parent)
 
-    class _LazyImportAs:
+    class _LazyImportAs(algutils.switcheroo.Switcheroo):
         def __init__(self, name: str, as_name: str) -> None:
+            super().__init__()
+
             self._name = name
             self._as_name = as_name
 
-        def __getattr__(self, attribute_name: str) -> Any:
-            attribute = getattr(globals()[self._name], attribute_name)
-            globals()[self._as_name] = globals()[self._name]
-            return attribute
+        def _load_switch_to(self, _unused_attribute_name: str) -> Any:
+            lazy_import_or_module_or_obj = deep_getattr.deep_getattr(_Everything._Globals(), self._name)
+            return deep_getattr.deep_getattr(_Everything._Globals(), self._name)
 
         @staticmethod
         def _lazy_import_as(name: str, as_name: str) -> None:
@@ -107,5 +115,15 @@ class _Everything:
 
 
 _Everything.lazy_import_everything()
+
 _Everything.lazy_import_as("numpy", "np")
 _Everything.lazy_import_as("pandas", "pd")
+
+if "nptyping" in globals():
+    # python3 -c 'import string, nptyping; print([as_name for as_name in dir(nptyping) if as_name[0] in string.ascii_uppercase])'
+    NPTYPING_TYPE_NAMES = ['Bool', 'Bool8', 'Byte', 'Bytes', 'Bytes0', 'CDouble', 'CFloat', 'CLongDouble', 'CLongFloat', 'CSingle', 'Character', 'Complex', 'Complex128', 'Complex64', 'ComplexFloating', 'DType', 'DataFrame', 'Datetime64', 'Double', 'Flexible', 'Float', 'Float16', 'Float32', 'Float64', 'Floating', 'Half', 'Inexact', 'Int', 'Int0', 'Int16', 'Int32', 'Int64', 'Int8', 'IntC', 'IntP', 'Integer', 'InvalidArgumentsError', 'InvalidDTypeError', 'InvalidShapeError', 'InvalidStructureError', 'LongComplex', 'LongDouble', 'LongFloat', 'LongLong', 'NDArray', 'NPTypingError', 'Number', 'Object', 'Object0', 'RecArray', 'Shape', 'Short', 'SignedInteger', 'Single', 'SingleComplex', 'Str0', 'String', 'Structure', 'Timedelta64', 'UByte', 'UInt', 'UInt0', 'UInt16', 'UInt32', 'UInt64', 'UInt8', 'UIntC', 'UIntP', 'ULongLong', 'UShort', 'Unicode', 'UnsignedInteger', 'Void', 'Void0']
+    for as_name in NPTYPING_TYPE_NAMES:
+        _Everything.lazy_import_as(f"nptyping.{as_name}", as_name)
+
+
+from typing import *
